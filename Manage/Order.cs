4109b33e93aadba0bcbe5076d4c40ba47715170c
Andrew26nin <tran26@mail.ru>
2019-02-13 13:30:00 +0300

    Data
    Реализовал подключение к Postgres

---
+++ Manage/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+
+{
+   public class Order
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Number { get; set; }
+
+        [MaxLength(300)]
+        [Required]
+        public string Description { get; set; }
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+
+        public Client Client { get; set; }
+
+    }
+}
